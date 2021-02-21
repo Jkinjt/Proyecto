e866b8354380913dce95615c6b66a7091380b510
joseangelmarinmoreno <60347715+josang1567@users.noreply.github.com>
2021-02-21 11:00:55 +0100

    modificaciones de order
    adicion de varias clases

---
+++ Proyecto2ª evaluacion/Restaurante/src/Order/MainControllerOrder.java
@@ -0,0 +1,88 @@
+package Order;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import client.Client;
+
+public class MainControllerOrder implements IMaincontrollerOrder {
+
+	@Override
+	public Order newOrder(Client c, LocalDateTime id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order Changerorder(Client c) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order Changerorder(LocalDate d) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order Changerorder(Client c, LocalDate d) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void DeleteOrder(Client c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void DeleteOrder(LocalDate d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void DeleteOrder(Client c, LocalDate d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int Cashtoday() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int CashThisMonth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int CashTotal() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Order ViewOrdersNotPayed() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order VierOrdersPendingDelivered() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void SaveAllAndClose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
