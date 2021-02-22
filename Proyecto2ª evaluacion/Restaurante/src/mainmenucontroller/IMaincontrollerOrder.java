b82c319352f4ba3e9bafcc18bacf903c982e20d9
joseangelmarinmoreno <60347715+josang1567@users.noreply.github.com>
2021-02-22 18:54:14 +0100

    mainmenucontrol

---
+++ Proyecto2ª evaluacion/Restaurante/src/mainmenucontroller/IMaincontrollerOrder.java
@@ -0,0 +1,35 @@
+package mainmenucontroller;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import Order.Order;
+import client.Client;
+
+public interface IMaincontrollerOrder {
+	public Order newOrder(Client c, LocalDateTime id) ; 
+	
+	public Order Changerorder(Client c);
+	
+	public Order Changerorder(LocalDate d);
+	
+	public Order Changerorder(Client c, LocalDate d);
+	
+	public void DeleteOrder(Client c);
+	
+	public void DeleteOrder(LocalDate d);
+	
+	public void DeleteOrder(Client c, LocalDate d);
+	
+	public int Cashtoday();
+	
+	public int CashThisMonth();
+	
+	public int CashTotal();
+	
+	public Order ViewOrdersNotPayed();
+	
+	public Order VierOrdersPendingDelivered();
+	
+	public void SaveAllAndClose();
+}
