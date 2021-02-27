ca297830542c25f0a42db196dc5980d00b56d28b
joseangelmarinmoreno <joseangelmarin1234@gmail.com>
2021-02-27 17:22:23 +0100

    modificacion menu ordenes

---
+++ Proyecto2ª evaluacion/Restaurante/src/menuMenus/MensajesOrder.java
@@ -0,0 +1,125 @@
+package menuMenus;
+
+public class MensajesOrder {
+	// meter todos los mensajes de los menus aqui
+	public static void menuopciones() {
+		System.out.println("+--------------------------+");
+		System.out.println("| 1-Modificar Cliente      |");
+		System.out.println("+--------------------------+");
+		System.out.println("| 2-Modificar productos    |");
+		System.out.println("+--------------------------+");
+		System.out.println("| 3-Modificar direccion    |");
+		System.out.println("+--------------------------+");
+		System.out.println("| 4-Modificar estado pago  |");
+		System.out.println("+--------------------------+");
+		System.out.println("| 5-Modificar estado envio |");
+		System.out.println("+--------------------------+");
+		System.out.println("| 0-Salir                  |");
+		System.out.println("+--------------------------+");
+	}
+	public static void insertar() {
+		System.out.println("+----------------------+");
+		System.out.println("| Seleccione una opcion|");
+		System.out.println("+----------------------+");
+	}
+	
+	//-----------------------------------------------------------
+	
+	public static void insertarClien() {
+		System.out.println("+-------------------------+");
+		System.out.println("| Inserte un nuevo cliente|");
+		System.out.println("+-------------------------+");
+	}
+	public static void mClient() {
+		System.out.println("+--------------------+");
+		System.out.println("| Cliente modificado |");
+		System.out.println("+--------------------+");
+	}
+	public static void menucli() {
+		System.out.println("+---------------------+");
+		System.out.println("| 1-buscar por nombre |");
+		System.out.println("+---------------------+");
+		System.out.println("| 2-buscar por DNI    |");
+		System.out.println("+---------------------+");
+		System.out.println("| 0-Salir             |");
+		System.out.println("+---------------------+");
+	}
+	public static void clientenombre() {
+		System.out.println("+---------------------------------+");
+		System.out.println("| Inserte el nombre del cliente |");
+		System.out.println("+---------------------------------+");
+	}
+	public static void clientedni() {
+		System.out.println("+------------------------------+");
+		System.out.println("| Inserte el dni del cliente |");
+		System.out.println("+------------------------------+");
+	}
+	public static void newcliente() {
+		System.out.println("+--------------------------+");
+		System.out.println("| Inserte un nuevo cliente |");
+		System.out.println("+--------------------------+");
+	}
+	
+	//-----------------------------------------------------------
+	
+	
+	
+	
+	public static void menupdro() {
+		System.out.println("+---------------------+");
+		System.out.println("| 1-Añadir producto   |");
+		System.out.println("+---------------------+");
+		System.out.println("| 2-Eliminar producto |");
+		System.out.println("+---------------------+");
+		System.out.println("| 0-Salir             |");
+		System.out.println("+---------------------+");
+	}
+	public static void insertarproductos() {
+		System.out.println("+-----------------------+");
+		System.out.println("|Introduzca el producto |");
+		System.out.println("+-----------------------+");
+	}
+	
+	public static void mprod() {
+		System.out.println("+-----------------------+");
+		System.out.println("| Productos modificados |");
+		System.out.println("+-----------------------+");
+	}
+	
+	//-----------------------------------------------------------	
+	
+	public static void maddr() {
+		System.out.println("+----------------------+");
+		System.out.println("| Direccion modificada |");
+		System.out.println("+----------------------+");
+	}
+	public static void insertardirecc() {
+		System.out.println("+-------------------------+");
+		System.out.println("| Introduzca una direccion|");
+		System.out.println("+-------------------------+");
+	}
+	
+	//-----------------------------------------------------------	
+	
+	public static void mdeli() {
+		System.out.println("+---------------------------+");
+		System.out.println("| Estado de pago modificado |");
+		System.out.println("+---------------------------+");
+	}
+	
+	//-----------------------------------------------------------
+	
+	public static void mpay() {
+		System.out.println("+----------------------------+");
+		System.out.println("| Estado de envio modificado |");
+		System.out.println("+----------------------------+");
+	}
+	
+	//-----------------------------------------------------------
+	
+	public static void salida() {
+		System.out.println("+----------+");
+		System.out.println("| SALIENDO |");
+		System.out.println("+----------+");		
+	}
+}
