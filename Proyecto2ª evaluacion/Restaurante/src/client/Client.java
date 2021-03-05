ac33c80e7a899bf622eff1f9e904a5970d38c04c
joseangelmarinmoreno <joseangelmarin1234@gmail.com>
2021-03-05 09:44:53 +0100

    .

--- Proyecto2ª evaluacion/Restaurante/src/client/Client.java
+++ Proyecto2ª evaluacion/Restaurante/src/client/Client.java
@@ -28,18 +28,23 @@ public class Client extends Person {
 	public String[] getAddress() {
 		return address;
 	}
+	
 	public void setAddress(String[] address) {
 		this.address = address;
 	}
+	
 	public int[] getOrders() {
 		return orders;
 	}
+	
 	public void setOrders(int[] orders) {
 		this.orders = orders;
 	}
+	
 	public int getPoints() {
 		return points;
 	}
+	
 	public void setPoints(int points) {
 		this.points = points;
 	}
