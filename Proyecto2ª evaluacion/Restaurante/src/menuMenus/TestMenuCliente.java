13ddb2769105814772417ad7b5d3b3b1a28d7089
joseangelmarinmoreno <joseangelmarin1234@gmail.com>
2021-03-06 17:16:15 +0100

    tests menus

---
+++ Proyecto2ª evaluacion/Restaurante/src/menuMenus/TestMenuCliente.java
@@ -0,0 +1,14 @@
+package menuMenus;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMenuCliente {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Proyecto2ª evaluacion/Restaurante/src/menuMenus/TestMenuOrdenes.java
@@ -0,0 +1,14 @@
+package menuMenus;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMenuOrdenes {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
