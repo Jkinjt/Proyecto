dcc3f2efbc6ae8e6305dd6ea4d1cbace3fa7585c
jkinjt <l02jitoj@gmail.com>
2021-03-06 10:37:39 +0100

    Cambiado demenus

---
+++ Proyecto2ª evaluacion/Restaurante/src/menuMenus/IMenuClientRepository.java
@@ -0,0 +1,21 @@
+package menuMenus;
+
+import client.Client;
+
+public interface IMenuClientRepository {
+	//buscar cliente por dni
+	public Client SearhClientBydni(int dni);
+	
+	//buscar cliente por nombre
+	public Client SearhClientByname(String name);
+	
+	//actualizar cliente
+	public Client UpdateClient(Client client);
+	
+	// añadir cliente
+	public void addClient(Client cliente);
+	
+	// eliminar cliente
+	
+	public void removeClient(Client cliente);
+} 
