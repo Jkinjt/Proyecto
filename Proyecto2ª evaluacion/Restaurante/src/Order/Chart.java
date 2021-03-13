42fd7025502c68fa108358ba3f71fd20735cc18f
jkinjt <l02jitoj@gmail.com>
2021-03-13 19:42:08 +0100

    carrito

--- Proyecto2ª evaluacion/Restaurante/src/Order/Chart.java
+++ Proyecto2ª evaluacion/Restaurante/src/Order/Chart.java
@@ -9,6 +9,7 @@ import client.Client;
 
 public class Chart {
 	public static Chart _instance;
+	private static float discont=0.10f;
 	private Client client;
 	private List<Product> products=new ArrayList<Product>();
 	private double total;
@@ -83,8 +84,26 @@ public class Chart {
 	}
 	public void totalCalculation() {
 		if(products.size()>1) {
+		for (int i = 0; i < products.size(); i++) {
+			if(!products.get(i).isYesOffer()) {
+				Product temporal=products.get(i).matchOffer((ArrayList) products);
+				for (int j = 0; j < products.size(); j++) {
+					if(temporal!=null&&!products.get(j).isYesOffer()&&temporal.equals(products.get(j))) {
+						products.get(j).setYesOffer(true);
+						j=products.size();
+					}
+					
+				}
+			}
+		}
+		
 			for (Product product : products) {
-				total+=product.getPrice();
+				if(product.isYesOffer()) {
+					total+=product.getPrice()*(1-discont);
+				}else {
+					total+=product.getPrice();	
+				}
+				
 			}
 		}
 	}
